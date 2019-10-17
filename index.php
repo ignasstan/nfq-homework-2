03425b1ef89d9411093417744350dd288dcfd4a6
ignasstan <ignassgit@gmail.com>
2019-10-17 23:01:50 +0300

    homework suitcss

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css"/>
+    <title>Frontend homework Suit-css</title>
+</head>
+<body>
+
+<div class="Container">
+    <div class="Alert Alert--warning ">
+        <h1 class='Alert-heading'>Incorrect email address.</h1>
+        <p class='Alert-text'>The email you entered does not belong to any InterNations account. <br>
+            Please try again. If you forget the email address with which you registered, please
+            <a href="#" class='Alert-text--email'>lorem@ipsum.org.</a>
+        </p>
+    </div>
+    <div class="Alert-break"></div>
+    <div class="Alert Alert--success">
+        <h1 class='Alert-heading'>Thank you!</h1>
+        <p class='Alert-text'>You have successfully sent the invitation at
+            <a href="#" class='Alert-text--email'>lorem@ipsum.org.</a><br>
+            All you have to do now is accept the invitation and start connecting with members of our community..
+        </p>
+    </div>
+    <div class="Alert-break"></div>
+    <button class="Button" type="button">CONTINUE TO NEXT PAGE</button>
+</div>
+
+</body>
+</html>
